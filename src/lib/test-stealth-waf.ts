d789944cae94bf877fdb8f6cba03dccbdc63ff3c
ruthvikgurrala <gurrala141@gmail.com>
2026-05-30 12:15:38 +0530

    Fix TypeScript error in test script for production build

--- src/lib/test-stealth-waf.ts
+++ src/lib/test-stealth-waf.ts
@@ -12,7 +12,7 @@ async function testStealth() {
     Object.defineProperty(navigator, 'webdriver', { get: () => false });
     Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
     Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-    window.chrome = { runtime: {} } as any;
+    (window as any).chrome = { runtime: {} } as any;
   });
 
   const page = await context.newPage();
